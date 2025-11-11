5f4f2036952eb6d74571c15d3731b0fe276c9fa9
gustavodscruz <gustavodiasdsc@gmail.com>
2025-11-11 20:15:59 -0300

    init project

---
+++ src/main/java/com/burnoutinhos/burnoutinhos_api/BurnoutinhosApiApplication.java
@@ -0,0 +1,13 @@
+package com.burnoutinhos.burnoutinhos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnoutinhosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurnoutinhosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/burnoutinhos/burnoutinhos_api/BurnoutinhosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burnoutinhos.burnoutinhos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurnoutinhosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
