dbf5cd6bc0b7ef237fe6e76eb37222f009a0b59f
Gustavo Dias da Silva Cruz <RM556448@fiap.com.br>
2025-11-18 01:53:50 +0000

    Merged PR 4: fix

--- src/main/java/com/burnoutinhos/burnoutinhos_api/config/FirebaseConfig.java
+++ src/main/java/com/burnoutinhos/burnoutinhos_api/config/FirebaseConfig.java
@@ -14,8 +14,10 @@ import java.net.http.HttpResponse;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!test")
 public class FirebaseConfig {
 
     @Value("${firebase.credentials}")
